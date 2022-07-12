c6c899814363f63dfd14e56ec8cf51ffe01fe0c5
Mohammad Mahdi Bigdeli <93078516+MohamdMahdiBigdeli@users.noreply.github.com>
2022-07-12 16:11:01 +0430

    Update API.py
    س

--- API.py
+++ API.py
@@ -1,9 +1,9 @@
 import json
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-from dao.Database import ReadDB
+from dao.Database import read_SQL
 
 
 @api_view(['''GET'''])
 def get_counters(request):
-    return HttpResponse(json.dumps(ReadDB(), default=lambda o: o.__dict__), content_type='''application/json''')
+    return HttpResponse(json.dumps(read_SQL('''counters'''), default=lambda o: o.__dict__), content_type='''application/json''')

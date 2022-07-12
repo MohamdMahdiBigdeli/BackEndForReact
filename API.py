499f12b83ea02b63b1b0269453643c4891b1a211
Mohammad Mahdi Bigdeli <mohammadmahdi.bigdeli@yahoo.com>
2022-07-12 16:46:49 +0430

    start SQL

--- API.py
+++ API.py
@@ -1,9 +1,9 @@
 import json
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-from dao.Database import read_Access
+from dao.Database import ReadDB
 
 
 @api_view(['''GET'''])
 def get_counters(request):
-    return HttpResponse(json.dumps(read_Access('''counters'''), default=lambda o: o.__dict__), content_type='''application/json''')
+    return HttpResponse(json.dumps(ReadDB(), default=lambda o: o.__dict__), content_type='''application/json''')
